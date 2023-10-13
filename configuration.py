45dc91a6419de45ac7c1491cd4a6cfd8a43fcf9c
Alex Sudakov <asudakov@samokat.ru>
2023-10-13 14:59:15 +0300

    second commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://fdde1aed-c519-4d6e-a24d-d5912ee5d071.serverhub.praktikum-services.ru"
+CREATE_ORDER = "/api/v1/orders"
+GET_ORDER = "/api/v1/orders/track"
+
---
+++ requests_function.py
@@ -0,0 +1,17 @@
+import requests
+import configuration
+import data
+
+
+def post_new_order():
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,
+                         json=data.order_body,
+                         headers=data.headers)
+
+
+def get_order(track_number):
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER,
+                        params={"t": track_number})
+#response2 = get_order()
+#print(response2.json())
+
